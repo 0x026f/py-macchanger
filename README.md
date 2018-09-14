88ea23395d485f05866d105ba72db661d77ec186
kpg_02 <kevinpigar@gmail.com>
2018-09-14 14:18:16 +0200

    1.1
    If is running with python 2 exit the program (python 3 needed)
    
    README.md well written

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-#Python Mac changer for linux
-A python mac changer for linux
+# py-macchanger
+<a href="https://docs.python-guide.org/starting/install3/linux/"><img src="https://images-na.ssl-images-amazon.com/images/I/61SA0Wq1P1L.png" width="80"> </a>
+
+### Features Version 1.01
+- Python 3
+- Linux only (at the moment)
+- super user needed
+
+### Instructions
+**1.** Download the repository, you can use the command below if you have git installed:
+
+<code>git clone https://github.com/peleon02/py-macchanger</code>
+
+
+**2.** Make sure that you have python 3 installed, just type python3 in your terminal
+and you will see an interpreter if it is installed, if you don't have it, click in the python logo at the beginning
+
+**3.** Go to the folder where you have the repository cloned, make sure you are in the folder which the repository is and use (of course it must be unziped, if you used git clone it should be already unziped)
+
+<code>cd py-macchanger-master</code>
+
+**4.** Now you have to run the program like <code>sudo python3 py-macchanger.py</code> and the arguments (check the section below)
+
+### Arguments
+######   You can use the short one with one "-" or you can use the long one with two "--"
+
+| Argument| Help |
+| ------ | ------ |
+| -a / --about |Use it alone to know about me and the program |
+| -m / --mac |Pass your new MAC (must be 6 pairs of alphanumerics characters) separated with ":" example <code>00:11:22:33:44:55</code> |
+| -i / --interface |Pass the interface which you wanna change the MAC, to know your interface use ifconfig (if you can't run it install <code>net-tools</code>) |
+| -r / --random |Pass it to generate random MACs  |
+
+#### Combination of arguments
+  **(examples only with short version but you can use long too)**
+
+- -a
+
+   <code>sudo python3 py-macchanger -a</code>
+
+- -r -i  example:
+
+  <code>sudo python3 py-macchanger -r -i eth0</code> (remember this is my interface it might not be the same as yours)
+- -m -i example:
+
+  <code>sudo python3 py-macchanger -m 00:14:22:91:56:37</code> (remember this a random mac as example)<code> -i eth0 </code>(remember this is my interface it might not be the same as yours) the whole code below:
+
+  <code>sudo python3 py-macchanger -m 00:14:22:91:56:37 -i eth0 </code>
--- py-macchanger.py
+++ py-macchanger.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 This tool change your mac until you restart your pc or get it back in other way
 
 #author peleon02 aka kpg_02
 #date 2018-09-13
-#version 1.0
+#version 1.01
 #Tested in python 3.x
 
 LICENSE:
-This software is distributed under the GNU General Public License version 3 (GPLv3)
+This software is distributed under the GNU General Public License version 3
 LEGAL NOTICE:
 THIS SOFTWARE IS PROVIDED FOR EDUCATIONAL USE ONLY!
 IF YOU ENGAGE IN ANY ILLEGAL ACTIVITY
 THE AUTHOR DOES NOT TAKE ANY RESPONSIBILITY FOR IT.
 BY USING THIS SOFTWARE YOU AGREE WITH THESE TERMS.
 """
-
+import sys
 import platform
 import subprocess
 import argparse
 import re
 from random import choice, randint
+
+if (sys.version_info > (3, 0)):
+    pass
+else:
+    print("This program was written for python 3")
+    exit()
 
 
 def get_arguments():
@@ -39,8 +46,8 @@ is used as argument"""
                         help='Display iformation about me (the developer)',
                         action="store_true")
     parser.add_argument('-r', '--random',
-                        help='Create a random MAC, to use this argument you' +
-                              'mustn´t use -m',
+                        help='Create a random MAC,' +
+                             'to use this argument you mustn´t use -m',
                         action="store_true")
     return parser.parse_args()
 
@@ -72,8 +79,8 @@ def change_mac(interface, new_mac):
 
 def CurrentMac():
     """Check the current MAC, taking it from the ifconfig + interface output
-and taking only the MAC with Regular Expresions in this case \w it takes any
-alphanumeric character"""
+and taking only the MAC with Regular Expresions in this case
+\w it takes any alphanumeric character"""
     output = subprocess.check_output(["ifconfig " +
                                       args.interface], shell=True)
     current_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(output))
@@ -85,9 +92,10 @@ def main():
         print("This tool cannot be used if you are" +
               "not running a Linux Distribution")
         exit()
-    if (args.about is True):  # Check if the -a / --about argument is passed
-        print('[*] Hi, I´m peleon02 aka kpg_02, the developer of this tool')
-        print('[*] Version 1.0, it was devoloped for python 3')
+    if (args.about is True):  # Check if the -a/--about argument is passed
+        print('[*] Hi, I´m peleon02 aka kpg_02,' +
+              'the developer of this tool')
+        print('[*] Version 1.01, it was devoloped for python 3')
         print('[*] Any suggestion for more tools' +
               'written in python are welcome')
     elif (args.random is True and args.interface is not None):
@@ -106,7 +114,8 @@ def main():
             print("[*] Script created by peleon02")
         else:
             print("[-] Something went wrong")
-    elif (args.about is False and args.mac is None or args.interface is None):
+    elif (args.about is False and args.mac is None or
+          args.interface is None):
         # The above code checks if About MAC and Interface are used,
         # and if they aren´t show the following message
         print("[-] You didn´t pass a MAC address or an Interface")
@@ -116,17 +125,19 @@ def main():
         print("[-] Make sure you use -i / " +
               "--interface (Name of the interface) for the interface")
         print("[-] You can use -a/--about to know more about me")
-    elif (re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(args.mac)) is None):
+    elif (re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",
+          str(args.mac)) is None):
         print("[-] You didn´t pass a valid MAC address")
         print("[-] Remember a MAC address must have 6 pairs of " +
               "alphanumerics characters")
         exit()
     elif(args.mac is not None and args.interface is not None):
-        # The above code check if the MAC argument and the interface argument
-        # aren´t empty
+        # The above code check if the MAC argument
+        # aren´t empty and the interface argument
             current_mac0 = CurrentMac()
             print("[+] Your current MAC for the interface " +
-                  str(args.interface) + " is " + str(current_mac0.group(0)))
+                  str(args.interface) + " is " +
+                  str(current_mac0.group(0)))
             change_mac(args.interface, args.mac)
             current_mac1 = CurrentMac()
             if current_mac0.group(0) != current_mac1.group(0):
@@ -136,6 +147,7 @@ def main():
                 print("[*] Script created by peleon02")
             else:
                 print("[-] Something went wrong")
+                print("[-] Make sure that you are running it as root")
 
     else:
         print("[-] Something went wrong")
@@ -148,3 +160,4 @@ if __name__ == '__main__':
     except Exception:
         print("[-] Something went wrong")
         print("[-] It might be a wrong interface name")
+        print("[-] If ifconfig is not found try to install net-tools")
